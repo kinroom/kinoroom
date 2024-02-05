bc3dbabbe356a3e5ae9a194df9b12dc63c94da85
LittleMoki <steamlittle659@gmail.com>
2024-02-05 09:07:09 +0500

    Deploying to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:'/kinoroom/',
-  build: {
-    outDir: 'build' // Change the output directory to 'build'
-  }
+  base: '/kinoroom/'
 })
